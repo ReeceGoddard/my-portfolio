3fc285f941ef4e2fc6f6bb98bfcc5d391bd21d0e
Reece Goddard <reecegoddard@me.com>
2023-06-04 12:38:14 -0400

    add more currency types

--- apps/currency-dash/src/types/index.ts
+++ apps/currency-dash/src/types/index.ts
@@ -19,6 +19,31 @@ export interface CurrencyEntry {
 
 export interface CurrencyData {
     date: string;
-    selectedCurrency: CurrencyEntry;
+    baseCurrency: CurrencyEntry;
     conversions: CurrencyEntry[];
+}
+
+export interface CurrencyHistory {
+    baseCurrency: CurrencyEntry;
+    data: CurrencyData[];
+}
+
+export interface BaseCurrency {
+    currencyCode: string;
+    label: string;
+}
+
+export interface CurrencyConversion {
+    currencyCode: string;
+    conversionHistory: RateForDate[];
+}
+
+export interface RateForDate {
+    date: string;
+    rate: number;
+}
+
+export interface ConversionsHistoryForCurrency {
+    baseCurrency: BaseCurrency;
+    currencyConversions: CurrencyConversion[];
 }
