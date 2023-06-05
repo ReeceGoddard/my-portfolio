8c0cb16f804887eda74483eecfd6f43a9f3fba5e
Reece Goddard <reecegoddard@me.com>
2023-06-05 17:04:46 -0400

    add main layout component with UI slots

--- apps/currency-dash/src/layouts/MainLayout.tsx
+++ apps/currency-dash/src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Children, ReactElement, ReactNode } from 'react';
 import './MainLayout.css';
 
 type SlotProps = {
-    name: 'sidebar' | 'content';
+    name: 'selector' | 'content';
     children: ReactNode;
 };
 
@@ -14,13 +14,13 @@ type MainLayoutProps = {
 
 export const MainLayout: React.FC<MainLayoutProps> & { Slot: React.FC<SlotProps> } = ({ children }) => {
     const childrenArray = Children.toArray(children) as unknown as React.ReactElement[];
-    const sidebarSlot = childrenArray.find(child => child?.props?.name === 'sidebar');
+    const selectorSlot = childrenArray.find(child => child?.props?.name === 'selector');
     const contentSlot = childrenArray.find(child => child?.props?.name === 'content');
 
     return (
         <main>
-            <div className="sidebar">{sidebarSlot?.props?.children}</div>
             <div className="content">{contentSlot?.props?.children}</div>
+            <div className="selector">{selectorSlot?.props?.children}</div>
         </main>
     );
 };
