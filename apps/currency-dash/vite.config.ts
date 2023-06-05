b7621d63fd1980b315bc8bb3f11352348cbbd262
Reece Goddard <reecegoddard@me.com>
2023-06-05 16:59:18 -0400

    add CSS modules and aliases to vite config

--- apps/currency-dash/vite.config.ts
+++ apps/currency-dash/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+
+type ViteConfigInput = { mode: string; command: string };
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const viteConfig = (args: ViteConfigInput) => {
+    const generateScopedName = args.mode === 'production' ? '[hash:base64:8]' : '[name]__[local]__[hash:base64:4]';
+
+    return defineConfig({
+        resolve: {
+            alias: {
+                '@assets': path.resolve(__dirname, './assets'),
+                '@services': path.resolve(__dirname, './services'),
+            },
+        },
+        css: {
+            modules: {
+                localsConvention: 'camelCase',
+                generateScopedName,
+                hashPrefix: 'prefix',
+            },
+        },
+        plugins: [react()],
+    });
+};
+
+export default viteConfig;
