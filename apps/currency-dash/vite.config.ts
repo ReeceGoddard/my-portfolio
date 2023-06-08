0d8b5bb9c6cdc2ca85eacb3e98bda34f1af06d2a
Reece Goddard <reecegoddard@me.com>
2023-06-08 11:21:18 -0400

    add nesting and server config to vite config

--- apps/currency-dash/vite.config.ts
+++ apps/currency-dash/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
+import postcssNesting from 'postcss-nesting';
 
 type ViteConfigInput = { mode: string; command: string };
 
@@ -9,6 +10,15 @@ const viteConfig = (args: ViteConfigInput) => {
     const generateScopedName = args.mode === 'production' ? '[hash:base64:8]' : '[name]__[local]__[hash:base64:4]';
 
     return defineConfig({
+        plugins: [react()],
+        server: {
+            watch: {
+                usePolling: true,
+            },
+            host: true,
+            strictPort: true,
+            port: 5173,
+        },
         resolve: {
             alias: {
                 '@assets': path.resolve(__dirname, './assets'),
@@ -21,8 +31,10 @@ const viteConfig = (args: ViteConfigInput) => {
                 generateScopedName,
                 hashPrefix: 'prefix',
             },
+            postcss: {
+                plugins: [postcssNesting],
+            },
         },
-        plugins: [react()],
     });
 };
 
