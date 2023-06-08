5f09cd0bb905e18e7275dc738f3762943dbe6232
Reece Goddard <reecegoddard@me.com>
2023-06-08 11:20:06 -0400

    fix error page message handling

--- apps/currency-dash/src/pages/ErrorPage.tsx
+++ apps/currency-dash/src/pages/ErrorPage.tsx
@@ -1,14 +1,37 @@
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 export const ErrorPage = () => {
     const error = useRouteError();
-    console.error(error);
 
-    return (
-        <div>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occured.</p>
-            <p>{error.statusText || error.message}</p>
-        </div>
-    );
+    if (isRouteErrorResponse(error)) {
+        if (error.status === 401) {
+            //TODO: Add specific error handling
+        } else if (error.status === 404) {
+            //TODO: Add specific error handling
+        }
+
+        return (
+            <div id="error-page">
+                <h1>Oops! {error.status}</h1>
+                <p>{error.statusText}</p>
+                {error.data?.message && (
+                    <p>
+                        <i>{error.data.message}</i>
+                    </p>
+                )}
+            </div>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div id="error-page">
+                <h1>Oops! Unexpected Error</h1>
+                <p>Something went wrong.</p>
+                <p>
+                    <i>{error.message}</i>
+                </p>
+            </div>
+        );
+    } else {
+        return <></>;
+    }
 };
