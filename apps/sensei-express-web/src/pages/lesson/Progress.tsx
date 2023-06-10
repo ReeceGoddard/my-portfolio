44dfc756dde054b56db5e43eec7f9761c526718e
Reece Goddard <reecegoddard@me.com>
2023-06-09 21:48:43 -0400

    add Progress component

---
+++ apps/sensei-express-web/src/pages/lesson/Progress.tsx
@@ -0,0 +1,31 @@
+import { HTMLProps } from 'react';
+import styles from './Progress.module.css';
+
+type Segment = {
+    result?: boolean;
+};
+
+export interface ProgressProps extends HTMLProps<HTMLDivElement> {
+    segments: Segment[];
+    currentSegment: number;
+}
+
+export const Progress = ({ segments, currentSegment, className, ...rest }: ProgressProps) => {
+    return (
+        <div className={`${styles.progress} ${className}`} {...rest}>
+            <div className={styles.completionLabel}>
+                Q{currentSegment} <span className={styles.fade}>of</span> {segments.length}
+            </div>
+            <div className={styles.segments}>
+                {segments.map(segment => (
+                    <div
+                        key={Math.random()}
+                        className={`${styles.segment} ${
+                            segment.result ? (segment.result === true ? styles.correct : styles.incorrect) : ''
+                        }`}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
+};
