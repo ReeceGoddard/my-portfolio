89ecaeb532cca8ef14e08f26240a62198df22e8a
Reece Goddard <reecegoddard@me.com>
2023-06-11 13:28:01 -0400

    add animation to multiple choice selection

--- apps/sensei-express-web/src/pages/lesson/Choice.tsx
+++ apps/sensei-express-web/src/pages/lesson/Choice.tsx
@@ -1,13 +1,17 @@
-import { HTMLProps, useEffect } from 'react';
+import { useAnimate } from 'framer-motion';
 import styles from './Choice.module.css';
+import { HTMLProps } from 'react';
 
 interface ChoiceProps extends HTMLProps<HTMLButtonElement> {
     label: string;
     choiceNumber: number;
+    isCorrectAnswer: boolean;
     onChoiceSelected: (choice: string) => void;
 }
 
-export const Choice = ({ label, choiceNumber, onChoiceSelected, className, ...rest }: ChoiceProps) => {
+export const Choice = ({ label, choiceNumber, isCorrectAnswer, onChoiceSelected, className, ...rest }: ChoiceProps) => {
+    const [scope, animate] = useAnimate();
+
     const handleMouseMove = (event: React.MouseEvent<HTMLButtonElement>) => {
         const { clientX, clientY } = event;
         const { left, top } = event.currentTarget.getBoundingClientRect();
@@ -18,17 +22,36 @@ export const Choice = ({ label, choiceNumber, onChoiceSelected, className, ...re
     };
 
     const selectChoice = () => {
-        //TODO: Animate button styles before selecting
+        const rgbVar = isCorrectAnswer ? '--bold-green-rgb-vals' : '--japan-red-rgb-vals';
+        const duration = 0.3;
+
+        animate(
+            scope.current,
+            {
+                boxShadow: [`0 0 0 0px rgb(var(${rgbVar}) / 0.2)`, `0 0 400px 200px rgb(var(${rgbVar}) / 0)`],
+                borderColor: [null, `rgb(var(${rgbVar}))`],
+                color: [null, `rgb(var(${rgbVar}))`],
+            },
+            { duration }
+        );
+
+        animate(
+            `div.${styles.hotkey}`,
+            { background: [null, `rgb(var(${rgbVar}))`], color: ['white', 'white'] },
+            { duration }
+        );
+
         onChoiceSelected(label);
     };
 
     return (
         <button
             className={`${styles.choice} ${className ? className : ''}`}
+            ref={scope}
             onMouseMove={handleMouseMove}
             {...rest}
             type={'button'}
-            onClick={() => selectChoice()}
+            onClick={selectChoice}
         >
             <div className={styles.hoverGradient}></div>
             <div className={styles.label}>{label}</div>
