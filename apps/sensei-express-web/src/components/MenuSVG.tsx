786c37f16bb3a15d5620f3ce99cf612123d011f3
Reece Goddard <reecegoddard@me.com>
2023-06-12 12:51:11 -0400

    move fill from rect to svg element

--- apps/sensei-express-web/src/components/MenuSVG.tsx
+++ apps/sensei-express-web/src/components/MenuSVG.tsx
@@ -2,10 +2,10 @@ import { SVGProps } from 'react';
 
 export const MenuSVG = (props: SVGProps<SVGSVGElement>) => {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <rect x="3" y="6" width="18" height="2" rx="0.5" fill="black" />
-            <rect x="3" y="11" width="18" height="2" rx="0.5" fill="black" />
-            <rect x="3" y="16" width="18" height="2" rx="0.5" fill="black" />
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <rect x="3" y="6" width="18" height="2" rx="0.5" />
+            <rect x="3" y="11" width="18" height="2" rx="0.5" />
+            <rect x="3" y="16" width="18" height="2" rx="0.5" />
         </svg>
     );
 };
