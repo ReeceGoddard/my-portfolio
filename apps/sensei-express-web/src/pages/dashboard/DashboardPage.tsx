0ede48bb6457da9fa1445a8bfc2d9816fe0a2703
Reece Goddard <reecegoddard@me.com>
2023-06-12 10:40:47 -0400

    use dashboard option component, add start sound

--- apps/sensei-express-web/src/pages/dashboard/DashboardPage.tsx
+++ apps/sensei-express-web/src/pages/dashboard/DashboardPage.tsx
@@ -1,25 +1,18 @@
-import { Link } from 'react-router-dom';
 import styles from './DashboardPage.module.css';
 import { MenuSVG } from '@/components/MenuSVG';
-import { PencilSVG } from '@/components/PencilSVG';
-import { MultipleChoiceSVG } from '@/components/MultipleChoiceSVG';
 import { LogoSVG } from '@/components/LogoSVG';
 import { useEffect, useRef } from 'react';
 import hoverSound from '@assets/sounds/hover.wav';
+import startSound from '@assets/sounds/start.wav';
+import { Option } from './Option';
 
 export const DashboardPage = (): JSX.Element => {
-    const audioElementRef = useRef<HTMLAudioElement | null>(null);
+    const hoverAudioElementRef = useRef<HTMLAudioElement | null>(null);
+    const startAudioElementRef = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
-        if (audioElementRef.current) audioElementRef.current.muted = false;
+        if (hoverAudioElementRef.current) hoverAudioElementRef.current.muted = false;
     }, []);
-
-    const playHoverSound = async () => {
-        if (audioElementRef.current) {
-            audioElementRef.current.currentTime = 0;
-            audioElementRef.current.play();
-        }
-    };
 
     return (
         <div className={styles.pageContainer}>
@@ -27,7 +20,7 @@ export const DashboardPage = (): JSX.Element => {
                 <header className={styles.card}>
                     <h1 className={styles.mainHeading}>
                         <LogoSVG width={80} height={80} />
-                        Sensei Express
+                        Sensei Academy
                     </h1>
                     <button className={styles.actions}>
                         <MenuSVG />
@@ -36,30 +29,26 @@ export const DashboardPage = (): JSX.Element => {
                 <section>
                     <h2 className={styles.sectionHeading}>Lessons</h2>
                     <div className={styles.options}>
-                        <Link className={styles.option} to={'/lesson/multi'} onMouseEnter={playHoverSound}>
-                            <MultipleChoiceSVG />
-                            <div className={styles.label}>Vowels</div>
-                        </Link>
-                        <Link className={styles.option} to={'/lesson/writing'} onMouseEnter={playHoverSound}>
-                            <PencilSVG />
-                            <div className={styles.label}>Vowels</div>
-                        </Link>
+                        <div>
+                            <span>Vowels Only —</span> <span>Multi</span> | <span>Writing</span> | <span>Mixed</span>
+                        </div>
+                        <Option label="Vowels Only" icon="multipleChoice" to={'/lesson/multi'} />
+                        <Option label="Vowels Only" icon="writing" to={'/lesson/writing'} />
+                        <Option label="Basic Hiragana" icon="multipleChoice" to={'/lesson/multi'} />
+                        <Option label="Basic Hiragana" icon="writing" to={'/lesson/writing'} />
                     </div>
                 </section>
 
                 <section>
                     <h2 className={styles.sectionHeading}>Boards</h2>
                     <div className={styles.options}>
-                        <Link className={styles.option} to={'/board/hiragana'} onMouseEnter={playHoverSound}>
-                            Hiragana board
-                        </Link>
-                        <Link className={styles.option} to={'/board/katakana'} onMouseEnter={playHoverSound}>
-                            Katakana board
-                        </Link>
+                        <Option label="Hiragana Board" icon="board" to={'/board/hiragana'} />
+                        <Option label="Katakana Board" icon="board" to={'/board/katakana'} />
                     </div>
                 </section>
             </div>
-            <audio ref={audioElementRef} src={hoverSound} />
+            <audio ref={hoverAudioElementRef} src={hoverSound} />
+            <audio ref={startAudioElementRef} src={startSound} />
         </div>
     );
 };
