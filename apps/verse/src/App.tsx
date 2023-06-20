5450ff95b418ba018e859f37ef8d9c122e4a3bd9
Reece Goddard <reecegoddard@me.com>
2023-06-20 14:11:57 -0400

    add local storage long-term cache

--- apps/verse/src/App.tsx
+++ apps/verse/src/App.tsx
@@ -1,19 +1,28 @@
 import { lazy } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { QueryClient } from '@tanstack/react-query';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { authorProfileLoader } from './pages/author-profile/AuthorProfile.loader';
 import './App.css';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 const Authors = lazy(() => import('./pages/authors/Authors'));
 const AuthorProfile = lazy(() => import('./pages/author-profile/AuthorProfile'));
+
+const FIVE_DAYS = 1000 * 60 * 60 * 24 * 5;
 
 function App() {
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
-                staleTime: Infinity,
+                staleTime: FIVE_DAYS,
+                cacheTime: FIVE_DAYS,
             },
         },
+    });
+
+    const persister = createSyncStoragePersister({
+        storage: window.localStorage,
     });
 
     const router = createBrowserRouter([
@@ -29,9 +38,9 @@ function App() {
     ]);
 
     return (
-        <QueryClientProvider client={queryClient}>
+        <PersistQueryClientProvider client={queryClient} persistOptions={{ persister, maxAge: FIVE_DAYS }}>
             <RouterProvider router={router} />
-        </QueryClientProvider>
+        </PersistQueryClientProvider>
     );
 }
 
