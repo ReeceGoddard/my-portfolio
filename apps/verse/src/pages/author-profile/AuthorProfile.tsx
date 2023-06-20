03a7f1c715d5377c1527cae4682e482cdb7dcb92
Reece Goddard <reecegoddard@me.com>
2023-06-20 19:02:41 -0400

    add author profile

--- apps/verse/src/pages/author-profile/AuthorProfile.tsx
+++ apps/verse/src/pages/author-profile/AuthorProfile.tsx
@@ -2,21 +2,71 @@ import { useQuery } from '@tanstack/react-query';
 import styles from './AuthorProfile.module.css';
 import { getAuthorProfileQuery } from '../../api/getAuthorProfile';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Poem } from '../../types';
 
 const AuthorProfile = () => {
     const { authorName } = useParams<keyof { authorName: string }>() as { authorName: string };
     const { data: poems } = useQuery(getAuthorProfileQuery(authorName as string));
+    const [poemsToggleStatus, setPoemsToggleStatus] = useState<Map<number, boolean>>(new Map<number, boolean>());
+    // const [selectedPoem, setSelectedPoem] = useState<Poem | null>(null);
+
+    useEffect(() => {
+        const titles: [[number, boolean]] = poems?.map((poem, index) => [index, false]) ?? [];
+        const map: Map<number, boolean> = new Map(titles);
+
+        setPoemsToggleStatus(map);
+    }, [poems]);
 
     return (
-        <div className={styles.authors}>
-            <h1>{poems !== undefined && poems[0].author}</h1>
-            <div className={styles.authorsGrid}>
-                {poems?.map(poem => (
-                    <div className={styles.poem} key={poem.title}>
-                        {poem.title}
-                    </div>
-                ))}
+        <div className={styles.authorProfile}>
+            <h1>Poems by {poems !== undefined && poems[0].author}</h1>
+            <div className={styles.poemsGrid}>
+                {poems
+                    ?.sort((a, b) => (a.title > b.title ? 1 : a.title < b.title ? 0 : 0))
+                    .map((poem, index) => (
+                        <div
+                            className={`${styles.poem} ${
+                                poemsToggleStatus.get(index) ? styles.poemVisible : styles.poemHidden
+                            }`}
+                            key={poem.title}
+                        >
+                            <button
+                                className={styles.togglePoem}
+                                onClick={() =>
+                                    setPoemsToggleStatus(
+                                        new Map(poemsToggleStatus.set(index, !poemsToggleStatus.get(index) || false))
+                                    )
+                                }
+                            >
+                                {poem.title}
+                                <svg
+                                    className={styles.chevronIcon}
+                                    viewBox="0 0 24 24"
+                                    fill="black"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M12 17.121L19.061 10.061L16.939 7.93897L12 12.879L7.06104 7.93897L4.93904 10.061L12 17.121Z" />
+                                </svg>
+                            </button>
+                            <div className={styles.poemLinesWrapper}>
+                                <div className={styles.poemLines}>
+                                    {poem.lines.map(line => (
+                                        <p className={styles.line} key={line}>
+                                            {line}
+                                        </p>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
             </div>
+            {/* <div className={`${styles.selectedPoemOverlay} ${selectedPoem !== null ? styles.overlayVisible : ''}`}>
+                <button onClick={() => setSelectedPoem(null)}>Close</button>
+                {selectedPoem?.lines.map(line => (
+                    <div key={line}>{line}</div>
+                ))}
+            </div> */}
         </div>
     );
 };
