a3bf09811c83425a61a826a4c726b3e187216909
Reece Goddard <reecegoddard@me.com>
2023-06-20 15:07:30 -0400

    add search to authors list page

--- apps/verse/src/pages/authors/Authors.tsx
+++ apps/verse/src/pages/authors/Authors.tsx
@@ -2,19 +2,34 @@ import { useQuery } from '@tanstack/react-query';
 import { authorsQuery } from '../../api/getAuthors';
 import styles from './Authors.module.css';
 import { Link } from 'react-router-dom';
+import { ChangeEvent, useState } from 'react';
 
 const Authors = () => {
     const { data } = useQuery(authorsQuery);
+    const [searchText, setSearchText] = useState<string>('');
+
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(event.target.value);
+    };
 
     return (
         <div className={styles.authors}>
-            <h1>Authors</h1>
+            <h1 className={styles.heading}>Poets</h1>
+            <input
+                className={styles.search}
+                type="text"
+                onChange={handleSearchChange}
+                value={searchText}
+                placeholder="Search..."
+            />
             <div className={styles.authorsGrid}>
-                {data?.map(author => (
-                    <Link className={styles.author} key={author} to={`/author/${author}`}>
-                        {author}
-                    </Link>
-                ))}
+                {data
+                    ?.filter(author => author.toLowerCase().includes(searchText.toLowerCase()))
+                    .map(author => (
+                        <Link className={styles.author} key={author} to={`/author/${author}`}>
+                            {author}
+                        </Link>
+                    ))}
             </div>
         </div>
     );
