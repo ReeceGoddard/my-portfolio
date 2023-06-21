74eac00632037bc448d8dcb8cf991dad3a706297
Reece Goddard <reecegoddard@me.com>
2023-06-21 18:49:38 -0400

    update results page component

--- apps/sensei-express-web/src/pages/results/ResultsPage.tsx
+++ apps/sensei-express-web/src/pages/results/ResultsPage.tsx
@@ -1,33 +1,69 @@
-import { useMemo } from 'react';
-import { Lesson, Question } from '../lesson/types';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useLessonContext } from '@/providers/LessonContext';
+import { QuestionResult } from './QuestionResult';
+import styles from './ResultsPage.module.css';
+import { AnimatePresence, motion } from 'framer-motion';
 
-type QuestionWithAnswer = {
-    userAnswer: string;
-    isCorrect: boolean;
-} & Question;
-
-interface ResultsPageProps {
-    questionsWithAnswers: QuestionWithAnswer[];
-}
+const calculateScoreMessage = (percentCorrect: number) => {
+    if (percentCorrect === 100) {
+        return "Perfect! Maybe it's time to try a harder lesson?";
+    } else if (percentCorrect >= 90) {
+        return 'Great job!';
+    } else if (percentCorrect >= 70) {
+        return "Not bad! You're getting there.";
+    } else {
+        return 'Try again and see if you can do better!';
+    }
+};
 
 export const ResultsPage = () => {
-    const lesson = useLocation().state as Lesson;
+    const { currentLesson, clearLesson } = useLessonContext();
 
-    const numOfCorrectAnswers = useMemo(() => {
-        return lesson.questions.reduce((prev, curr) => prev + (curr.isCorrect ? 1 : 0), 0);
-    }, [lesson]);
+    const questionsWithAnswers = currentLesson?.filter(question => question.userAnswer) || [];
+    const numOfCorrectAnswers = currentLesson?.reduce((prev, curr) => prev + (curr.isCorrect ? 1 : 0), 0) || 0;
+    const percentCorrect = (questionsWithAnswers.length / numOfCorrectAnswers) * 100;
 
-    const quantityMessage =
-        numOfCorrectAnswers > 0
-            ? `Well done! You got ${numOfCorrectAnswers} correct.`
-            : `Looks like you had trouble with that one. Try again!`;
+    const scoreMessage = calculateScoreMessage(percentCorrect);
 
     return (
-        <div>
-            <pre>{JSON.stringify(lesson, null, 3)}</pre>
-            <div>{quantityMessage}</div>
-            <Link to={'/dashboard'}>Back to dashboard</Link>
-        </div>
+        <AnimatePresence>
+            <motion.div
+                initial={{ translateY: -10, opacity: 0 }}
+                animate={{ translateY: 0, opacity: 1 }}
+                transition={{ duration: 0.8, ease: 'easeOut' }}
+                className={styles.resultsPageContainer}
+            >
+                <div className={styles.scoreMessage}>{scoreMessage}</div>
+                <div className={styles.resultsHeader}>
+                    <h1 className={styles.mainHeading}>Lesson Results</h1>
+                    <h2 className={styles.subHeading}>
+                        {numOfCorrectAnswers} / {questionsWithAnswers.length}
+                    </h2>
+                </div>
+
+                <div className={styles.results}>
+                    {questionsWithAnswers.map((question, index) => (
+                        <QuestionResult
+                            key={question.question.charID}
+                            questionWithAnswer={question}
+                            questionNumber={index + 1}
+                        />
+                    ))}
+                    {questionsWithAnswers.length === 0 ? (
+                        <div className={styles.noAnswersLabel}>No results to show</div>
+                    ) : null}
+                </div>
+                <section className={styles.postLesson}>
+                    <div className={styles.actions}>
+                        <Link to={'/dashboard'} onClick={clearLesson}>
+                            Done
+                        </Link>
+                        <Link className={styles.ghost} to={'/lesson/multi'} onClick={clearLesson}>
+                            Repeat lesson
+                        </Link>
+                    </div>
+                </section>
+            </motion.div>
+        </AnimatePresence>
     );
 };
