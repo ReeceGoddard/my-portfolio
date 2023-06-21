14b211fc4d32573566e321f4df80eb5ef1101d7e
Reece Goddard <reecegoddard@me.com>
2023-06-21 18:51:30 -0400

    update lesson page with context and animation

--- apps/sensei-express-web/src/pages/lesson/LessonPage.tsx
+++ apps/sensei-express-web/src/pages/lesson/LessonPage.tsx
@@ -5,33 +5,34 @@ import { Suspense, useEffect, useState } from 'react';
 import { Progress } from './Progress';
 import styles from './LessonPage.module.css';
 import { useLessonContext } from '@/providers/LessonContext';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
 
 export const LessonPage = () => {
-    const { initLesson, currentLesson, answerCurrentQuestion, clearLesson } = useLessonContext();
+    const { initLesson, currentLesson, currentQuestion, answerCurrentQuestion } = useLessonContext();
     const lesson = useLoaderData() as Lesson;
-    // const [answers, setAnswers] = useState<QuestionType[]>(Array(lesson.questions.length).fill({}));
     const navigate = useNavigate();
     const [activeQuestion, setActiveQuestion] = useState(1);
+    const [scope, animate] = useAnimate();
 
     useEffect(() => {
-        initLesson(lesson);
+        initLesson(lesson.questions);
     }, []);
 
     const finishLesson = () => {
-        navigate('/results', { state: lesson });
+        animate(
+            scope.current,
+            {
+                blur: ['0px', '20px'],
+                opacity: [1, 0],
+            },
+            { duration: 0.3 }
+        ).then(() => {
+            navigate('/results', { state: lesson });
+        });
     };
 
     const handleAnswer = (userAnswer: string) => {
-        const isCorrect = userAnswer.toLowerCase() === lesson.questions[activeQuestion - 1].answer.toLowerCase();
         answerCurrentQuestion(userAnswer);
-        // setAnswers(prev =>
-        //     prev.map((answer, i) => {
-        //         if (i === activeQuestion - 1) {
-        //             return { ...lesson.questions[i], isCorrect, userAnswer };
-        //         }
-        //         return answer;
-        //     })
-        // );
 
         if (activeQuestion < lesson.questions.length) {
             setActiveQuestion(activeQuestion + 1);
@@ -43,28 +44,47 @@ export const LessonPage = () => {
     return (
         <Suspense>
             <Await resolve={lesson} errorElement={<p>Error loading lesson.</p>}>
-                {currentLesson ? (
-                    <>
-                        <Progress
-                            className={styles.progress}
-                            segments={currentLesson.questions.map(question => {
-                                return { result: question.isCorrect };
-                            })}
-                            currentSegment={activeQuestion}
-                            endLesson={finishLesson}
-                        />
-                        <div key={currentLesson.id + Math.random()} className={styles.questions}>
-                            <Question
-                                key={currentLesson.questions[activeQuestion - 1].question}
-                                className={styles.question}
-                                question={currentLesson.questions[activeQuestion - 1].question}
-                                answer={currentLesson.questions[activeQuestion - 1].answer}
-                                onAnswer={userAnswer => handleAnswer(userAnswer)}
-                                choices={currentLesson.questions[activeQuestion - 1].choices ?? []}
+                <AnimatePresence>
+                    {currentLesson ? (
+                        <motion.div
+                            ref={scope}
+                            initial={{
+                                filter: 'blur(20px)',
+                                opacity: 0,
+                                rotate: '2deg',
+                                scale: 1.1,
+                            }}
+                            animate={{
+                                filter: 'blur(0px)',
+                                opacity: 1,
+                                rotate: '0deg',
+                                scale: 1,
+                            }}
+                            transition={{ duration: 0.8 }}
+                        >
+                            <Progress
+                                className={styles.progress}
+                                segments={currentLesson.map(question => {
+                                    return { result: question.isCorrect };
+                                })}
+                                currentSegment={activeQuestion}
+                                endLesson={finishLesson}
                             />
-                        </div>
-                    </>
-                ) : null}
+                            <div className={styles.questions}>
+                                {currentQuestion ? (
+                                    <Question
+                                        key={currentQuestion?.question.charID}
+                                        className={styles.question}
+                                        question={currentQuestion.question.question}
+                                        answer={currentQuestion.question.answer}
+                                        onAnswer={userAnswer => handleAnswer(userAnswer)}
+                                        choices={currentQuestion.question.choices ?? []}
+                                    />
+                                ) : null}
+                            </div>
+                        </motion.div>
+                    ) : null}
+                </AnimatePresence>
             </Await>
         </Suspense>
     );
