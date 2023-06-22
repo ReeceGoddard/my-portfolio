f976a5e38be952ea31891b4e060b2a1116f39417
Reece Goddard <reecegoddard@me.com>
2023-06-21 20:34:21 -0400

    clean up server index

--- apps/sensei-express-api/src/index.ts
+++ apps/sensei-express-api/src/index.ts
@@ -1,30 +1,43 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
-import express, { Request } from 'express';
-const app = express();
+import express, { NextFunction, Request, Router } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { router as lessonRoutes } from './lesson/lesson.routes.js';
-import { router as boardRoutes } from './board/board.routes.js';
 import { PrismaClient } from '@prisma/client';
 
-app.use(cors());
-app.use(express.json());
+import { router as lessonRoutes } from './lesson/lesson.routes.js';
+import { router as boardRoutes } from './board/board.routes.js';
+import { errorHandler } from './errorHandler.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 morgan.token('body', (req: Request) => {
     return JSON.stringify(req.body);
 });
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.disable('x-powered-by');
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 const prisma = new PrismaClient();
 
-const routerV1 = express.Router();
+const routerV1 = Router();
 routerV1.use('/lesson', lessonRoutes);
 routerV1.use('/board', boardRoutes);
+
 app.use('/v1', routerV1);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+//Generic error handler after all routes
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+    try {
+        await prisma.$connect();
+        console.log(`Connected to the database 🔌`);
+        console.log(`Server listening on port ${PORT} 🩺`);
+    } catch (error) {
+        console.error(`Failed to connect to the database:`, error);
+    }
 });
