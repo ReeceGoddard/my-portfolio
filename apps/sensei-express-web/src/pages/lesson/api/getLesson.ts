01b0d332cc9485951c044c80b3356e388bab6e49
Reece Goddard <reecegoddard@me.com>
2023-06-21 20:39:01 -0400

    clean up getLesson API

--- apps/sensei-express-web/src/pages/lesson/api/getLesson.ts
+++ apps/sensei-express-web/src/pages/lesson/api/getLesson.ts
@@ -1,62 +1,32 @@
 import { ExtractFnReturnType, QueryConfig } from '@lib/react-query';
-import { Lesson, Question } from '../types';
+import { GetLessonAPIResponse } from '../types';
+import { axios } from '@/lib/axios';
+import { useQuery } from '@tanstack/react-query';
 
-export type LessonType = 'writing' | 'multi';
+export type LessonType = 'writing' | 'multi' | 'mixed';
+export type LessonLevel = 'vowels' | 'basic' | 'intermediate' | 'full';
 
-const questionLibrary: Question[] = [
-    { id: '1', createdAt: 1, question: 'あ', answer: 'a' },
-    { id: '2', createdAt: 1, question: 'い', answer: 'i' },
-    { id: '3', createdAt: 1, question: 'う', answer: 'u' },
-    { id: '4', createdAt: 1, question: 'え', answer: 'e' },
-    { id: '5', createdAt: 1, question: 'お', answer: 'o' },
-];
-
-const getRandomChoices = (questionToIgnore: Question): string[] => {
-    return questionLibrary
-        .filter(libraryQuestion => libraryQuestion.answer !== questionToIgnore.answer)
-        .sort(() => Math.random() - 0.5)
-        .slice(0, 3)
-        .map(question => question.answer);
-};
-
-//TODO: Allow for flipping the lesson, where English letter is shown and 4 Hiragana chars as options
-export const getLesson = (type: LessonType = 'writing'): Promise<Lesson> => {
-    return new Promise<Lesson>(resolve => {
-        const lessonQuestions = questionLibrary.slice().sort(() => Math.random() - 0.5);
-
-        if (type === 'multi') {
-            const updatedQuestions = lessonQuestions.map(lessonQuestion => {
-                const choices: string[] = getRandomChoices(lessonQuestion);
-                choices.push(lessonQuestion.answer);
-                choices.sort(() => Math.random() - 0.5);
-                return { ...lessonQuestion, choices };
-            });
-
-            resolve({
-                id: '1',
-                createdAt: 1,
-                questions: updatedQuestions,
-            });
-        }
-
-        resolve({
-            id: '1',
-            createdAt: 1,
-            questions: lessonQuestions,
-        });
-    });
+export const getLesson = (
+    lessonLevel: LessonLevel,
+    lessonType: LessonType = 'multi'
+): Promise<GetLessonAPIResponse> => {
+    return axios.get(`/v1/lesson/hiragana/${lessonLevel}/${lessonType}`);
 };
 
 type QueryFnType = typeof getLesson;
 
-type UseLessonOptions = {
+type LessonQueryOptions = {
+    lessonLevel: LessonLevel;
+    lessonType: LessonType;
     config?: QueryConfig<QueryFnType>;
 };
 
-// export const useLesson = ({ config }: UseLessonOptions = {}) => {
-//     return useQuery<ExtractFnReturnType<QueryFnType>>({
-//         ...config,
-//         queryKey: ['lesson'],
-//         queryFn: getLesson,
-//     });
-// };
+export const lessonQuery = ({ config, lessonLevel, lessonType }: LessonQueryOptions) => ({
+    ...config,
+    queryKey: ['lesson'],
+    queryFn: () => getLesson(lessonLevel, lessonType),
+});
+
+export const useLesson = ({ config, lessonLevel, lessonType }: LessonQueryOptions) => {
+    return useQuery<ExtractFnReturnType<QueryFnType>>(lessonQuery({ config, lessonLevel, lessonType }));
+};
