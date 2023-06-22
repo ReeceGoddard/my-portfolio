1b063264785f97edcf8fb608578454dd890a978c
Reece Goddard <reecegoddard@me.com>
2023-06-22 12:43:19 -0400

    add label prop to switch component

--- apps/sensei-express-web/src/components/switch/Switch.tsx
+++ apps/sensei-express-web/src/components/switch/Switch.tsx
@@ -3,13 +3,19 @@ import styles from './Switch.module.css';
 interface SwitchProps {
     checked: boolean;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
+    label?: string;
 }
 
-export const Switch = ({ checked, onChange }: SwitchProps) => {
+export const Switch = ({ checked, onChange, label }: SwitchProps) => {
     return (
-        <>
+        <div className={styles.switchWrapper}>
             <input type="checkbox" id="switch" className={styles.switchInput} checked={checked} onChange={onChange} />
             <label className={styles.switchLabel} htmlFor="switch" />
-        </>
+            {label ? (
+                <label className={styles.label} htmlFor="switch">
+                    {label}
+                </label>
+            ) : null}
+        </div>
     );
 };
